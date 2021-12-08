4b34cedc76607f28a426475d2e3fa7be35806f91
Przemek Malak <przemek.malak@gmail.com>
2021-12-08 16:15:30 +0100

    Update days.py

--- days.py
+++ days.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def last_day_of_current_month():
     next_month = datetime.date.today().replace(day=28) + datetime.timedelta(days=4)
     return next_month - datetime.timedelta(days=next_month.day)
@@ -10,7 +11,12 @@ def first_day_of_current_month():
 
 def first_day_of_next_month():
     first_day_of_month = datetime.date.today().replace(day=1)
-    first_day_of_month = first_day_of_month.replace(month=first_day_of_month.month+1)
+    next_month=first_day_of_month.month+1
+    year = first_day_of_month.year
+    if next_month > 12:
+        next_month = 1
+        year = year + 1
+    first_day_of_month = first_day_of_month.replace(month=next_month, year=year)
     return first_day_of_month
 
 def today():
